aa75674bef96a4e53c0653f90a105e26caf5aeb2
sono <abdelrhmanstorm123@gmail.com>
2024-12-08 12:39:54 +0200

    Fix: Update base URL in vite.config.js for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,11 @@
-import path from "path"
-import react from "@vitejs/plugin-react"
-import { defineConfig } from "vite"
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import path from 'path'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/portfolio/',
+  base: './',
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
